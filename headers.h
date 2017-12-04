3fa4d332357292e96a97f6b94858085bd57aa5f9
kkhan01 <khanshan501@gmail.com>
2017-12-04 00:52:19 -0500

    Update headers.h

--- headers.h
+++ headers.h
@@ -9,12 +9,23 @@
 #include <sys/fcntl.h>
 #include <signal.h>
 
+
+
 int num_separated(char *line, char *limit);
+
 char **separate_commands(char *line, char *separator);
-char **parse_args(char *line, char *separator);
+
 int cd(char **args);
+
 int execute(char **args);
-void redirection(char * direction, int destination);
-int command(char * cmd);
-int path();
+
+void redir(char * direction, int destination);
+
+void pipin(char * first, char * second);
+
 char * trim(char * bush);
+
+int command(char * cmd);
+
+int path();
+
